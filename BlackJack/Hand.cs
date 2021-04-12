712f2bdd47a670383d668c97134af3bff4268218
ZeHan361 <68045400+ZeHan361@users.noreply.github.com>
2021-04-12 13:47:31 -0500

    Add files via upload

---
+++ BlackJack/Hand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackJack;
+
+namespace BlackJack
+{
+    class Hand
+    {
+        public static void Shuffle(ref Cards[]Deck)
+        {
+            Random random = new Random();
+            Cards card;
+            int number;
+        }
+    }
+}
