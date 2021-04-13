34db7abbb58436c0e811eb5877d10770034b0f61
bhogan3 <54568046+bhogan3@users.noreply.github.com>
2021-04-12 22:08:04 -0500

    Added player class

---
+++ BlackJack/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Player
+    {
+        private Hand hand;
+        //private int number;
+        public Player()
+        {
+            hand = new Hand();
+            //this.number = number;
+        }
+        public void Hit(Cards card)
+        {
+            //TODO: needs method to add cards to hand
+           // hand.add(card);
+        }
+        public bool Bust()
+        {
+           if (this.Points() > 21)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int Points()
+        {   
+            //TODO: needs a way to see cards in hand or should points be in hand?
+            //int points = 0;
+            //foreach (Cards card in this.hand.cards)
+            //{
+            //    points += card.point;
+            //}
+            //return points;
+            return 10;
+        }
+    }
+}
