122374befb0dcde837a49b160c6c044ef242e9c0
cunger3 <54676502+cunger3@users.noreply.github.com>
2021-04-15 11:49:07 -0500

    Added Dealer class

--- BlackJack/Dealer.cs
+++ BlackJack/Dealer.cs
@@ -1,6 +1,66 @@
 ﻿using System;
 using BlackJack;
-public class Dealer : Player
-{
 
+namespace BlackJack
+{
+    public class Dealer : Player
+    {
+        // The "Hole" card is the card at the beginning of the game that the other players can not see
+        private Card hole;
+        private bool holeRevealed = false;
+
+
+        public bool HoleRevealed
+        {
+            get => holeRevealed;
+            set => holeRevealed = value;
+        }
+
+        public Card Hole
+        {
+            get => hole;
+            set => hole = value;
+        }
+
+
+        public void PlayTurn()
+        {
+            if (this.Points() == 0)
+            {
+                // TODO: Waiting on Deck implementation
+                // this.Hit(Deck.ShuffleDraw());
+                this.DrawHole();
+                return;
+            }
+
+            if (this.HoleRevealed == false)
+            {
+                this.HoleRevealed = true;
+            }
+
+            if (this.Points() < 17)
+            {
+                // TODO: Waiting on Deck implementation
+                // this.Hit(Deck.ShuffleDraw());
+            }
+            else
+            {
+                // Stand
+                return;
+            }
+        }
+
+        public void DrawHole()
+        {
+            // TODO: Waiting on Deck implementation
+            // this.Hole = Deck.ShuffleDraw();
+        }
+
+        public void RevealHole()
+        {
+            this.Hit(this.Hole);
+            this.HoleRevealed = true;
+        }
+    }
 }
+
