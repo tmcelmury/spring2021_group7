198ff5305df5d0b17fec930e1d58d7bd674e58b3
bhogan3 <54568046+bhogan3@users.noreply.github.com>
2021-04-22 21:46:42 -0500

    Added Solitaire project

---
+++ Solitaire/Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire
+{
+    public class Card
+    {
+        private string suit;
+        private int value;
+        private string type;
+
+        public Card(string suit, string type, int value)
+        {
+            this.value = value;
+            this.suit = suit;
+            this.type = type;
+        }
+        public string getSuit() { return suit; }
+
+        override
+        public String ToString()
+        {
+            return this.type + " of " + this.suit;
+        }
+
+        public int getValue()
+        {
+            return this.value;
+        }
+
+
+
+    }
+
+}
