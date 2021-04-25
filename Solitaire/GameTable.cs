8707efdcf59a798a421dd612ed41ce2786e3215d
bhogan3 <54568046+bhogan3@users.noreply.github.com>
2021-04-25 18:37:06 -0500

    FaceUp and FaceDown put in lists

--- Solitaire/GameTable.cs
+++ Solitaire/GameTable.cs
@@ -12,62 +12,50 @@ namespace Solitaire
         public Stack<Card> faceUpDeck = new Stack<Card>();
 
         // game board rows
-        public List<Card> faceUp1 = new List<Card>();
-        public List<Card> faceUp2 = new List<Card>();
-        public List<Card> faceDown2 = new List<Card>();
-        public List<Card> faceUp3 = new List<Card>();
-        public List<Card> faceDown3 = new List<Card>();
-        public List<Card> faceUp4 = new List<Card>();
-        public List<Card> faceDown4 = new List<Card>();
-        public List<Card> faceUp5 = new List<Card>();
-        public List<Card> faceDown5 = new List<Card>();
-        public List<Card> faceUp6 = new List<Card>();
-        public List<Card> faceDown6 = new List<Card>();
-        public List<Card> faceUp7 = new List<Card>();
-        public List<Card> faceDown7 = new List<Card>();
         public List<Card> heartAce = new List<Card>();
         public List<Card> clubAce = new List<Card>();
         public List<Card> spadeAce = new List<Card>();
         public List<Card> diamondAce = new List<Card>();
 
-        //public List<List<Card>> faceUp = new List<List<Card>>();
-        //public List<List<Card>> faceDown = new List<List<Card>>();
+        public List<List<Card>> faceUp = new List<List<Card>>();
+        public List<List<Card>> faceDown = new List<List<Card>>();
 
 
         public GameTable()
         {
-            //for (int i = 0; i < 7; i++)
-            //{
-            //    faceUp[i] = new List<Card>();
-            //    faceDown[i] = new List<Card>();
-            //}
-            deck = new Deck().getDeck();
-            faceUp1.Add(deck.Pop());
-            faceUp2.Add(deck.Pop());
-            faceDown2.Add(deck.Pop());
-            faceUp3.Add(deck.Pop());
-            for (int i = 0; i < 2; i++){
-                faceDown3.Add(deck.Pop());
+            for (int i = 0; i <= 7; i++)
+            {
+                faceUp.Add(new List<Card>());
+                faceDown.Add(new List<Card>());
             }
-            faceUp4.Add(deck.Pop());
+            deck = new Deck().getDeck();
+            faceUp[1].Add(deck.Pop());
+            faceUp[2].Add(deck.Pop());
+            faceDown[2].Add(deck.Pop());
+            faceUp[3].Add(deck.Pop());
+            for (int i = 0; i < 2; i++)
+            {
+                faceDown[3].Add(deck.Pop());
+            }
+            faceUp[4].Add(deck.Pop());
             for (int i = 0; i < 3; i++)
             {
-                faceDown4.Add(deck.Pop());
+                faceDown[4].Add(deck.Pop());
             }
-            faceUp5.Add(deck.Pop());
+            faceUp[5].Add(deck.Pop());
             for (int i = 0; i < 4; i++)
             {
-                faceDown5.Add(deck.Pop());
+                faceDown[5].Add(deck.Pop());
             }
-            faceUp6.Add(deck.Pop());
+            faceUp[6].Add(deck.Pop());
             for (int i = 0; i < 5; i++)
             {
-                faceDown6.Add(deck.Pop());
+                faceDown[6].Add(deck.Pop());
             }
-            faceUp7.Add(deck.Pop());
+            faceUp[7].Add(deck.Pop());
             for (int i = 0; i < 6; i++)
             {
-                faceDown7.Add(deck.Pop());
+                faceDown[7].Add(deck.Pop());
             }
         }
         // displays the rows
@@ -87,14 +75,11 @@ namespace Solitaire
 
         public void StackFlip(int row)
         {
-            if (row == 2)
+            if (faceDown[row].Count != 0)
             {
-                if (faceDown2.Count != 0)
-                {
-                    Card c = faceDown2[0];
-                    faceDown2.Remove(c);
-                    faceUp2.Add(c);
-                }
+                Card c = faceDown[row][0];
+                faceDown[row].Remove(c);
+                faceUp[row].Add(c);
             }
         }
         public void Display()
@@ -107,72 +92,20 @@ namespace Solitaire
             }
             Console.WriteLine();
             // rows
-            Console.Write("1: ");
-            foreach (Card c in faceUp1)
+            for (int i = 1; i <= 7; i++)
             {
-                Console.Write(c.getDisplay());
+                Console.Write($"{i}: ");
+                foreach (Card c in faceDown[i])
+                {
+                    Console.Write("X ");
+                }
+                foreach (Card c in faceUp[i])
+                {
+                    Console.Write(c.getDisplay());
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
-            Console.Write("2: ");
-            foreach (Card c in faceDown2)
-            {
-                Console.Write("x ");
-            }
-            foreach (Card c in faceUp2)
-            {
-                Console.Write(c.getDisplay());
-            }
-            Console.WriteLine();
-            Console.Write("3: ");
-            foreach (Card c in faceDown3)
-            {
-                Console.Write("x ");
-            }
-            foreach (Card c in faceUp3)
-            {
-                Console.Write(c.getDisplay());
-            }
-            Console.WriteLine();
-            Console.Write("4: ");
-            foreach (Card c in faceDown4)
-            {
-                Console.Write("x ");
-            }
-            foreach (Card c in faceUp4)
-            {
-                Console.Write(c.getDisplay());
-            }
-            Console.WriteLine();
-            Console.Write("5: ");
-            foreach (Card c in faceDown5)
-            {
-                Console.Write("x ");
-            }
-            foreach (Card c in faceUp5)
-            {
-                Console.Write(c.getDisplay());
-            }
-            Console.WriteLine();
-            Console.Write("6: ");
-            foreach (Card c in faceDown6)
-            {
-                Console.Write("x ");
-            }
-            foreach (Card c in faceUp6)
-            {
-                Console.Write(c.getDisplay());
-            }
-            Console.WriteLine();
-            Console.Write("7: ");
-            foreach (Card c in faceDown7)
-            {
-                Console.Write("x ");
-            }
-            foreach (Card c in faceUp7)
-            {
-                Console.Write(c.getDisplay());
-            }
+
         }
     }
-
 }
