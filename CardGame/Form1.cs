0b0605d0f228708404cd00953d281afd7530a4d6
tmcelmury <trentamcelmury@gmail.com>
2021-04-27 16:57:12 -0500

    Main menu

--- CardGame/Form1.cs
+++ CardGame/Form1.cs
@@ -10,12 +10,26 @@ using System.Windows.Forms;
 
 namespace CardGame
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
 
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
